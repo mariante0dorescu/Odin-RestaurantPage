f8d6b05f26b2d90af1aa37aad16c16d141ba7c0c
mariante0dorescu <broinkus@gmail.com>
2023-06-24 21:18:44 +0300

    created webpack with sass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './styles/main.scss';
+
 function component() {
   const element = document.createElement('div');
   element.innerHTML = 'test';
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,49 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
+  entry: {
+    main: path.resolve(__dirname, 'src/index.js'),
   },
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
   },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    open: true, 
+    hot: true, 
+    compress: true,
+    historyApiFallback: true
+  },
+  module:{
+    rules:[
+      {
+        test: /\.scss$/i, // any file ending with .scss use ...
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack App Marian',
+      filename: 'index.html',
+    })
+  ]
 }
